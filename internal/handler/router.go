9769e586cc87371422463fa5026bca40f2d23678
melifaro <melifaro.nbk@gmail.com>
2025-09-21 20:23:00 +0300

    routing

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -14,10 +14,6 @@ func NewRouter(h *Handlers, auth service.Auth, logger *zap.Logger) chi.Router {
 	//r.Use(LoggingMiddleware(logger))
 
 	r.Group(func(r chi.Router) {
-		//r.Get("/ping", h.Ping)
-	})
-
-	r.Group(func(r chi.Router) {
 		//r.Use(authMiddleware(logger, auth))
 		r.Route("/api/user", func(r chi.Router) {
 			r.Post("/register", h.Register)
@@ -25,12 +21,15 @@ func NewRouter(h *Handlers, auth service.Auth, logger *zap.Logger) chi.Router {
 			r.Group(func(r chi.Router) {
 				r.Use(jwtauth.Verifier(auth.AuthProvider()))
 				r.Use(jwtauth.Authenticator(auth.AuthProvider()))
-				r.Post("/orders", h.CreateOrder)
-				r.Get("/orders", h.OrderList)
+				r.Route("/order", func(r chi.Router) {
+					r.Post("/", h.CreateOrder)
+					r.Get("/", h.OrderList)
+				})
+				r.Route("/balance", func(r chi.Router) {
+					r.Get("/", h.UserBalance)
+					r.Post("/withdraw", h.Withdraw)
+				})
 			})
-			//r.Post("/api/shorten", h.ShortenJSON)
-			//r.Post("/api/shorten/batch", h.BatchShorten)
-			//r.Get("/{id}", h.Redirect)
 		})
 	})
 
